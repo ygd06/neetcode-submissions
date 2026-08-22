20f6fd4d42c0977dec0ec0c1aa9413a29a6ba6a2
ygd06 <128312228+ygd06@users.noreply.github.com>
2026-08-22 13:59:59 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n == 0) return n;
+        vector<int>dp(n+2, 0);
+        dp[1] = 1;
+        for(int i = 2; i <= n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n+1];
+    }
+};
